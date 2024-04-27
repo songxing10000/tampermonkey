456538e9fb8fcb7bcda28ece8dfa05b41241be12
songxing10000 <songxing10000@live.cn>
2024-04-27 17:57:47 +0800

    up

--- New Userscript.user.js
+++ New Userscript.user.js
@@ -4,39 +4,29 @@
 // @version      2024-04-27
 // @description  try to take over the world!
 // @author       You
-// @match        *://*.jianshu.com/*
+// @match        *://*.jianshu.com/p/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=jianshu.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    // 在页面加载完成后运行的函数
+    window.addEventListener('load', function() {
+        // 删除 推荐的好看文章
+        document.querySelectorAll('section').forEach(function(section) {
+            if (section.textContent.startsWith("热门故事")||
+                section.textContent.includes("总资产") ||
+                section.textContent.includes("发现更多相似") ||
+                section.textContent.includes("全部评论")) {
 
-    // 定义一个方法来点击新增元素
-    function clickNewElement(element) {
-        var maxAttempts = 10; // 最大尝试次数
-        var attempt = 0; // 当前尝试次数
-
-        var interval = setInterval(function() {
-            // 选择按钮元素
-            var closeButton = document.querySelector('button._23ISFX-close');
-
-            // 检查按钮是否存在
-            if (closeButton) {
-                // 模拟点击按钮
-                closeButton.click();
-                console.log('成功点击关闭按钮');
-                clearInterval(interval); // 停止循环
             } else {
-                attempt++;
-                if (attempt >= maxAttempts) {
-                    console.error('超过最大尝试次数，未找到关闭按钮。');
-                    clearInterval(interval); // 停止循环
-                }
+                setTimeout(function() {
+                    section.parentNode.removeChild(section);
+                }, 1000);
             }
-        }, 100); // 100毫秒间隔
-
-    }
+        });
+    });
 
     // 创建一个 MutationObserver 实例
     var observer = new MutationObserver(function(mutationsList) {
@@ -45,10 +35,19 @@
             // 检查每个新增的节点
             if (mutation.type === 'childList') {
                 mutation.addedNodes.forEach(function(node) {
-                    // 检查是否是元素节点
-                    if (node.nodeType === Node.ELEMENT_NODE) {
-                        // 调用点击新增元素的方法
-                        clickNewElement(node);
+
+                    // 点击 扫码弹窗
+                    var closeButton = document.querySelector('button._23ISFX-close');
+                    if (closeButton) {
+                        // 模拟点击按钮
+                        closeButton.click();
+                        console.log('成功点击关闭按钮');
+
+                    }
+                    // 删除 热门故事
+                    if (node.textContent.startsWith("热门故事")) {
+                        node.parentNode.removeChild(node);
+                        console.log('已删除包含热门故事的 section 元素。');
                     }
                 });
             }
@@ -60,4 +59,6 @@
 
     // 开始观察 document 的变化
     observer.observe(document, config);
+
+
 })();
